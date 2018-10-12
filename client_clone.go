3fd81ba6a077d6f6da2394d6e4813ba862d589d7
Andrei Popescu <andrei.popescu@gmail.com>
2018-10-12 11:24:22 +0300

    bug fixing
    bug fixing

--- client_clone.go
+++ client_clone.go
@@ -1,16 +1,7 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+package main
 
-// +build go1.8
+import "fmt"
 
-package websocket
-
-import "crypto/tls"
-
-func cloneTLSConfig(cfg *tls.Config) *tls.Config {
-	if cfg == nil {
-		return &tls.Config{}
-	}
-	return cfg.Clone()
+func main() {
+	fmt.Println("hello world")
 }
